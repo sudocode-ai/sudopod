148d72c750e15f7a04b3d9cf6e26a9d079b6c9f4
Randy Song <ssh.randy@gmail.com>
2024-01-24 00:01:03 -0500

    yeet

--- server/scripts/startup_scripts.py
+++ server/scripts/startup_scripts.py
@@ -1,3 +1,4 @@
 STARTUP_SCRIPT = """#!/bin/bash
 usermod -aG sudo {username} 
+sudo docker pull us-west1-docker.pkg.dev/pods-staging/docker-images/sudopod-jupyter:latest
 sudo docker run -d -p {jupyter_port}:8888 --restart=always -u $(id -u {username}):$(id -g {username}) -v /home/{username}:/home/jovyan/ -e JUPYTER_TOKEN='{jupyter_access_token}' us-west1-docker.pkg.dev/pods-staging/docker-images/sudopod-jupyter:latest"""
