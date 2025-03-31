14f62ac120844a8293ab2f1fddbe2b43b7c21799
randy <ssh.randy@gmail.com>
2025-03-31 00:15:02 -0700

    setup missing fields properly when running prep-cluster

--- packages/shared/scripts/seed/postgres/seed-db.go
+++ packages/shared/scripts/seed/postgres/seed-db.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/uuid"
 
 	"github.com/e2b-dev/infra/packages/shared/pkg/db"
+	"github.com/e2b-dev/infra/packages/shared/pkg/keys"
 	"github.com/e2b-dev/infra/packages/shared/pkg/models/accesstoken"
 	"github.com/e2b-dev/infra/packages/shared/pkg/models/team"
 )
@@ -52,9 +53,48 @@ func main() {
 
 	email := config["email"].(string)
 	teamID := config["teamId"].(string)
-	accessToken := config["accessToken"].(string)
+	accessTokenStr := config["accessToken"].(string)
 	teamAPIKey := config["teamApiKey"].(string)
 	teamUUID := uuid.MustParse(teamID)
+
+	// Get or generate access token ID
+	var accessTokenUUID uuid.UUID
+	if accessTokenId, ok := config["accessTokenId"].(string); ok {
+		accessTokenUUID = uuid.MustParse(accessTokenId)
+	} else {
+		accessTokenUUID = uuid.New()
+	}
+
+	// Get or generate hashes and masks
+	hasher := keys.NewSHA256Hashing()
+
+	var accessTokenHash string
+	if hash, ok := config["accessTokenHash"].(string); ok {
+		accessTokenHash = hash
+	} else {
+		accessTokenHash = hasher.Hash([]byte(accessTokenStr))
+	}
+
+	var accessTokenMask string
+	if mask, ok := config["accessTokenMask"].(string); ok {
+		accessTokenMask = mask
+	} else {
+		accessTokenMask = "sk_e2b_***"
+	}
+
+	var teamAPIKeyHash string
+	if hash, ok := config["teamApiKeyHash"].(string); ok {
+		teamAPIKeyHash = hash
+	} else {
+		teamAPIKeyHash = hasher.Hash([]byte(teamAPIKey))
+	}
+
+	var teamAPIKeyMask string
+	if mask, ok := config["teamApiKeyMask"].(string); ok {
+		teamAPIKeyMask = mask
+	} else {
+		teamAPIKeyMask = "e2b_***"
+	}
 
 	// Open .e2b/config.json
 	user, err := database.Client.User.Create().SetEmail(email).SetID(uuid.New()).Save(ctx)
@@ -85,14 +125,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	// Create access token
-	_, err = database.Client.AccessToken.Create().SetUser(user).SetID(accessToken).Save(ctx)
+	_, err = database.Client.AccessToken.Create().
+		SetUser(user).
+		SetID(accessTokenUUID).
+		SetAccessToken(accessTokenStr).
+		SetAccessTokenHash(accessTokenHash).
+		SetAccessTokenMask(accessTokenMask).
+		Save(ctx)
 	if err != nil {
 		panic(err)
 	}
 
 	// Create team api key
-	_, err = database.Client.TeamAPIKey.Create().SetTeam(t).SetAPIKey(teamAPIKey).Save(ctx)
+	_, err = database.Client.TeamAPIKey.Create().
+		SetTeam(t).
+		SetAPIKey(teamAPIKey).
+		SetAPIKeyHash(teamAPIKeyHash).
+		SetAPIKeyMask(teamAPIKeyMask).
+		Save(ctx)
 	if err != nil {
 		panic(err)
 	}
